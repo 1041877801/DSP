afe4744cf3ccbc7813adabe5c8ff66a2b45940a1
1041877801 <64666346+1041877801@users.noreply.github.com>
2024-06-14 16:57:31 +0800

    Add files via upload

---
+++ best_res.sh
@@ -0,0 +1,4 @@
+python eval.py --dataset ENZYMES --depth 5 --t 100 --c 256 --load_embedding;
+python eval.py --dataset IMDB-BINARY --depth 1 --t 150 --c 128 --load_embedding;
+python eval.py --dataset PTC_MM --depth 2 --t 150 --c 4 --C 10 --load_embedding;
+python eval.py --dataset PTC_FR --depth 3 --t 250 --c 8 --C 10 --load_embedding;
